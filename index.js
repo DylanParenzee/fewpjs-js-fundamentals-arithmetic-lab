ed219d1b96fd6d078a6d5b9db3abb973a78cfc92
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-05 11:19:10 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+
+const random = Math.floor(Math.random() * 3)
+
+let num3 = 16
+let num4 = 6
+
+const mod = num3 % num4
+
+const max  = Math.max(12,4,2,1,20,4)
